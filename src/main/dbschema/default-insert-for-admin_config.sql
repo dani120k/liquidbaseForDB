0627b0dbe5fd5ba8f9b2bb8965c0ba41830d99af
forol <forolymp120@gmail.com>
2018-12-16 00:10:22 +0300

    /

--- src/main/dbschema/default-insert-for-admin_config.sql
+++ src/main/dbschema/default-insert-for-admin_config.sql
@@ -1,2 +1,6 @@
 INSERT INTO domain VALUES (1, 'domain_name');
+INSERT INTO org_unit VALUES (1, 'org_unit_name', 'description', 1);
+INSERT INTO users VALUES (1, 'login', 'password', '2018-01-08', 1);
+INSERT INTO user_info VALUES (1, 'email', 'fName', 'sName', 1);
+INSERT INTO task VALUES (1, 'desc', '1999-01-08',1, NULL);
 
